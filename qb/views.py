72efccc70a7c29c2af335efd95a596ec6dba3a21
MonikaShah <monikapatira@gmail.com>
2020-04-14 22:12:32 +0530

    Add files via upload

--- qb/views.py
+++ qb/views.py
@@ -28,29 +28,26 @@ def index(request):
     class AboutView(TemplateView):
         template_view = 'templates/School1617Codes/about.html'
 
-# def village_info(request,vcd):
-#     return render(request,'qb/templates/village_info.html',{vcd})
-
-
- 
- 
-# Generate counts of some of the school codes
-    # cursor = connection.cursor()
-
     district_selected = None
     subdistrict_selected = None
     village_selected = None
     school_selected = None
     schcd_selected = None
-    schcd = 0
-    dcd = 0
-    bcd =0
-    vcd=0
-    toal_popu =0
-    tot_m = 0
-    tot_f = 0
-    tot_work_f = 0
-    tot_work_m =0
+    villagecode_selected =None
+    schoolcode_selected =None
+
+    # schcd = 0
+    # dcd = 0
+    # bcd =0
+    # vcd=0
+    # toal_popu =0
+    # tot_m = 0
+    # tot_f = 0
+    # tot_work_f = 0
+    # tot_work_m =0
+    # vilcod=0
+    # schcod=0
+    # schmgt =0
     
     
     num_school = School1617Codes.objects.all().count()
@@ -61,15 +58,33 @@ def index(request):
     num_coed_school= SchoolGeneral1617.objects.filter(schtype='3').count()
     dname = School1617Codes.objects.order_by('distname').values('distname').distinct()
 
+    # bname= School1617Codes.objects.filter(distname=district_selected).distinct('block_name')
+
     if request.method == "POST":
-        # Filter parmaeters by selected region, but only on a POST
+        district_selected = None
+        subdistrict_selected = None
+        village_selected = None
+        school_selected = None
+        villagecode_selected = None
+        schoolcode_selected = None
+        dcd = 0
+        bcd =0
+        vcd=0
+        toal_popu =0
+        tot_m = 0
+        tot_f = 0
+        tot_work_f = 0
+        tot_work_m =0
+        schmgt =0
+        # district_selected = request.POST.get("data")
         district_selected = request.POST.get("dist_select")
         subdistrict_selected = request.POST.get("subdist_select")
         village_selected = request.POST.get("village_select")
         school_selected = request.POST.get("school_select")
-        vilcod = request.POST.get("vilcod")
-        # tot_p = cursor.callproc('tot_p',[vdd and tot_p])
-        
+        villagecode_selected = request.POST.get("villagecode_select")
+        schoolcode_selected = request.POST.get("schoolcode_select")
+        # showchart = request.POST.get("show")
+      
         
     context = {
             'num_school': num_school,
@@ -80,46 +95,38 @@ def index(request):
             
             
             'bname': School1617Codes.objects.filter(distname=district_selected).distinct('block_name'),
-            'vname' : School1617Codes.objects.filter(block_name=subdistrict_selected).distinct('village_name'),
-            'sname' : School1617Codes.objects.filter(village_name=village_selected).distinct('school_name'),
-            
+            'vname' : School1617Codes.objects.filter(block_name=subdistrict_selected).filter(distname=district_selected).distinct('village_name'),
+            'sname' : School1617Codes.objects.filter(village_name=village_selected).filter(block_name=subdistrict_selected).filter(distname=district_selected).distinct('school_name'),
+            'vilcod' : School1617Codes.objects.filter(village_name=village_selected).filter(block_name=subdistrict_selected).filter(distname=district_selected).distinct('village_code'),
+            'schcode' : School1617Codes.objects.filter(school_name=school_selected).filter(village_name=village_selected).filter(block_name=subdistrict_selected).filter(distname=district_selected).distinct('school_code'),
+                   
             'district_selected':district_selected,
             'subdistrict_selected':subdistrict_selected,
             'village_selected':village_selected,
             'school_selected':school_selected,
-            'vilcod':vilcod,
-                  
-        
-            'dcd':School1617Codes.objects.values_list('district_code',flat=True).get(school_name=school_selected),
-            'bcd':School1617Codes.objects.values_list('block_code',flat=True).get(school_name=school_selected),
-            'vcd':School1617Codes.objects.values_list('village_code',flat=True).get(school_name=school_selected),               
-            'schcd':School1617Codes.objects.values_list('school_code',flat=True).get(school_name=school_selected),
+            'villagecode_selected':villagecode_selected,
+            'schoolcode_selected':schoolcode_selected,
+            # 'showchart':showchart,
             
-            
-            'tot_popu':MahaDdw.objects.values_list('tot_p',flat=True).get(census_2011code=vilcod),
-            'tot_m':MahaDdw.objects.values_list('tot_m',flat=True).get(census_2011code=vilcod),
-            'tot_f':MahaDdw.objects.values_list('tot_f',flat=True).get(census_2011code=vilcod),
-            'tot_work_m':MahaDdw.objects.values_list('tot_work_m',flat=True).get(census_2011code=vilcod),
-            'tot_work_f':MahaDdw.objects.values_list('tot_work_f',flat=True).get(census_2011code=vilcod),
-           
-
-            # 'vdd':School1617Codes.objects.get_queryset
-            # 'tot_p': MahaDdw.objects.values_list('tot_p',flat=True).get(census_2011code=vdd),
-            # 'tot_popo': MahaDdw.objects.filter(census_2011code=vcd).distinct(toal_p),
-            # for tot_p in MahaDdw.objects.raw('SELECT tot_p FROM qb_mahaddw WHERE census_2011code = %s', [vcd]):
-            #     print(toal_p)
         
+            # 'dcd':School1617Codes.objects.values_list('district_code',flat=True).get(school_name=school_selected,village_name=village_selected,block_name=subdistrict_selected,distname=district_selected),
+            # 'bcd':School1617Codes.objects.values_list('block_code',flat=True).get(school_name=school_selected,village_name=village_selected,block_name=subdistrict_selected,distname=district_selected),
+               
+            # 'tot_popu':MahaDdw.objects.values_list('tot_p',flat=True).get(census_2011code=villagecode_selected),
+            # 'tot_m':MahaDdw.objects.values_list('tot_m',flat=True).get(census_2011code=villagecode_selected),
+            # 'tot_f':MahaDdw.objects.values_list('tot_f',flat=True).get(census_2011code=villagecode_selected),
+            # 'tot_work_m':MahaDdw.objects.values_list('tot_work_m',flat=True).get(census_2011code=villagecode_selected),
+            # 'tot_work_f':MahaDdw.objects.values_list('tot_work_f',flat=True).get(census_2011code=villagecode_selected),
+              
+            # 'schmgt':SchoolGeneral1617.objects.values_list('schmgt',flat=True).get(schcd=schoolcode_selected),
+            # 'schcat':SchoolGeneral1617.objects.values_list('schcat',flat=True).get(schcd=schoolcode_selected),
+            # 'schtype':SchoolGeneral1617.objects.values_list('schtype',flat=True).get(schcd=schoolcode_selected),
+            # 'lowclass':SchoolGeneral1617.objects.values_list('lowclass',flat=True).get(schcd=schoolcode_selected),
+            # 'highclass':SchoolGeneral1617.objects.values_list('highclass',flat=True).get(schcd=schoolcode_selected),
+            
         }
 
       
  # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context)
 
-  
-
-    # if form.is_valid():
-    #     dist_selected = form.cleaned_data['value']
-    #     values = request.POST.getlist()
-    #     print(values)
-    #     return render(request, 'index.html',{'dist_selected': dist_selected})
-        
